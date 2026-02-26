b9903456fd8c81dec592650e9427c291c31b1ae3
Jordan Alexander Suwito <jordansuwito@gmail.com>
2026-02-27 01:56:34 +0700

    second-commit

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { Toolbar, AppBar, Button, Box, Stack } from "@mui/material";
+import { Toolbar, AppBar, Button, Box, Stack, Avatar } from "@mui/material";
 import type { PropsWithChildren } from "react";
 import { useAppSelector } from "../hooks/useAppSelector";
 import { Link } from "react-router";
@@ -9,6 +9,32 @@ import { authActions } from "../store/authSlice";
 export function Layout(props: PropsWithChildren) {
     const userInfo = useAppSelector((state) => state.auth.userInfo);
     const dispatch = useAppDispatch();
+
+    function stringToColor(string: string) {
+        let hash = 0;
+        let i;
+
+        for (i = 0; i < string.length; i += 1) {
+            hash = string.charCodeAt(i) + ((hash << 5) - hash);
+        }
+
+        let color = "#";
+
+        for (i = 0; i < 3; i += 1) {
+            const value = (hash >> (i * 8)) & 0xff;
+            color += `00${value.toString(16)}`.slice(-2);
+        }
+
+        return color;
+    }
+
+    function getInitials(name: string) {
+        return name
+            .split(" ")
+            .map((n) => n[0])
+            .join("")
+            .toUpperCase();
+    }
 
     return (
         <Stack>
@@ -48,7 +74,7 @@ export function Layout(props: PropsWithChildren) {
                         )}
                     </Box>
 
-                    {/* RIGHT SIDE */}
+
                     <Box
                         flex={1}
                         display="flex"
@@ -68,12 +94,28 @@ export function Layout(props: PropsWithChildren) {
 
                         {userInfo && (
                             <>
-                                <Button
-                                    color="inherit"
-                                    sx={{ textTransform: "none" }}
-                                >
-                                    {userInfo.user.name}
-                                </Button>
+                                <Box display="flex" alignItems="center" gap={1}>
+                                    <Avatar
+                                        sx={{
+                                            bgcolor: stringToColor(
+                                                userInfo.user.name,
+                                            ),
+                                            color: "#fff",
+                                            fontWeight: 600,
+                                            width: 36,
+                                            height: 36,
+                                        }}
+                                    >
+                                        {getInitials(userInfo.user.name)}
+                                    </Avatar>
+
+                                    <Button
+                                        color="inherit"
+                                        sx={{ textTransform: "none" }}
+                                    >
+                                        {userInfo.user.name}
+                                    </Button>
+                                </Box>
 
                                 <Button
                                     color="error"
@@ -82,7 +124,6 @@ export function Layout(props: PropsWithChildren) {
                                     onClick={() => {
                                         dispatch(
                                             authActions.setUserInfo(undefined),
-                                            
                                         );
                                     }}
                                 >
